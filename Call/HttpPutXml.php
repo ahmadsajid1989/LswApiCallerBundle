d80f4cab77c46fd902c9c3f3c4384a6f36adbd1b
Ahmad Sajid <Ahmad Sajid>
2016-08-08 17:52:51 +0600

    added post xml and put xml method

---
+++ Call/HttpPutXml.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ahmad
+ * Date: 8/8/2016
+ * Time: 2:27 PM
+ */
+
+namespace Lsw\ApiCallerBundle\Call;
+
+
+class HttpPutXml extends CurlCall implements ApiCallInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function generateRequestData()
+    {
+        //dump($this->requestObject[0]);exit;
+        return $this->requestObject[0];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function parseResponseData()
+    {
+        if($this->asAssociativeArray) {
+            $xml = simplexml_load_string($this->responseData);
+            $json = json_encode($xml);
+            $this->responseObject = json_decode( $json, TRUE );
+        } else {
+            $this->responseObject = $this->responseData;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function makeRequest($curl, $options)
+    {
+
+        $curl->setopt(CURLOPT_URL, $this->url);
+        $curl->setopt(CURLOPT_POST, 1);
+        $curl->setopt(CURLOPT_POSTFIELDS, $this->generateRequestData());
+        $curl->setopt(CURLOPT_CUSTOMREQUEST, "PUT");
+        $curl->setoptArray($options);
+        $this->curlExec($curl);
+    }
+
+}
